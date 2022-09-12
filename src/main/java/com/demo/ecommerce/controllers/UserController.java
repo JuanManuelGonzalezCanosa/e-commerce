6ba2e2adc1a0e9424275dcaf9f86ad720a7d1747
juanmanuelgonzalezcanosa <juanmanuelgonzalezcanosaa@gmail.com>
2022-09-12 01:05:16 -0300

    Funciones de User Service/Controller

---
+++ src/main/java/com/demo/ecommerce/controllers/UserController.java
@@ -0,0 +1,62 @@
+package com.demo.ecommerce.controllers;
+
+
+import com.demo.ecommerce.entities.ShoppingCart;
+import com.demo.ecommerce.entities.User;
+import com.demo.ecommerce.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RequestMapping("/User")
+@RestController
+public class UserController {
+
+    @Autowired
+    public UserService service;
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public UserController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        return new ResponseEntity<User>(service.createUser(user), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public List<User> lstUser(){ return service.lstUser();}
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Integer id){
+        return service.getUserById(id);
+    }
+
+    @PutMapping("/put/{id}")
+    public boolean putUserById(@RequestBody User user, @PathVariable Integer id){
+        return service.putUserById(user, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean deteleUserById(@PathVariable Integer id){
+        return service.deleteUserById(id);
+    }
+
+    @PostMapping
+    public User userAddShoppingCart(@PathVariable Integer idUser, @PathVariable Integer idShoppingCart){
+        ResponseEntity<ShoppingCart> response = restTemplate.getForEntity("http://localhost:8080/shoppingCart/"+ idShoppingCart.toString(), ShoppingCart.class);
+        ShoppingCart shoppingCart = response.getBody();
+
+        return service.userAddShoppingCart(idUser, shoppingCart);
+    }
+
+}
--- src/main/java/com/demo/ecommerce/services/UserService.java
+++ src/main/java/com/demo/ecommerce/services/UserService.java
@@ -1,16 +1,57 @@
 package com.demo.ecommerce.services;
 
-
+import com.demo.ecommerce.entities.ShoppingCart;
+import com.demo.ecommerce.entities.User;
 import com.demo.ecommerce.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import java.util.List;
 
+@Service
+@Qualifier("UserService")
+public class UserService {
 
     @Autowired
     @Qualifier("IUserRepository")
     private IUserRepository repository;
+
+    public User createUser(User user) {return repository.save(user);}
+
+    public List<User> lstUser() {
+        return repository.findAll();
+    }
+
+    public User getUserById(Integer id) {
+        return repository.findById(id).get();
+    }
+
+
+    public boolean putUserById(User user, Integer id) {
+
+        User aux = this.getUserById(id);
+
+        aux.setName(user.getName());
+        aux.setPassword(user.getPassword());
+        aux.setRol(user.getRol());
+        aux.setShoppingCart(user.getShoppingCart());
+        aux.setActive(user.isActive());
+
+        repository.save(aux);
+        return  true;
+    }
+
+    public boolean deleteUserById(Integer id){
+        repository.deleteById(id);
+        return true;
+    }
+
+    public User userAddShoppingCart(Integer idUser, ShoppingCart shoppingCart){
+        User aux = this.getUserById(idUser);
+
+        aux.getShoppingCart().add(shoppingCart);
+
+        return repository.save(aux);
+    }
 }
